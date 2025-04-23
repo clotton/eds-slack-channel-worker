079a45aec6914a96481de954d6e959d8b4a48322
kovac <kovac@adobe.com>
2025-04-23 16:16:47 -0400

    feat: cache token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,24 +1,24 @@
-export const authenticate = async (env) => {
-  const authData = {
-    client_id: env.CLIENT_ID,
-    client_secret: env.CLIENT_SECRET,
-    grant_type: 'client_credentials',
-    resource: 'https://graph.microsoft.com',
-    scope: 'https://graphs.microsoft.com/.default'
-  };
-  const formData = new FormData();
-  for(let key in authData) {
-    formData.append(key, authData[key]);
-  }
+let cachedBearerToken = null;
 
-  const authUrl = 'https://login.microsoftonline.com/36fb8e1d-a891-493f-96d0-3038e4e9291c/oauth2/token';
-  const authRes = await fetch(authUrl, {
-    method: 'POST',
-    body: formData,
-  });
-  if (authRes.status === 200) {
-    const auth = await authRes.json();
-    return auth.access_token;
-  }
-  return null;
+export const authenticate = async (env) => {
+    if (cachedBearerToken) return cachedBearerToken;
+
+    const authData = {
+        client_id: env.CLIENT_ID,
+        client_secret: env.CLIENT_SECRET,
+        grant_type: 'client_credentials',
+        resource: 'https://graph.microsoft.com',
+        scope: 'https://graphs.microsoft.com/.default'
+    };
+    const formData = new URLSearchParams(authData);
+    const response = await fetch(env.AUTH_URL, {
+        method: 'POST',
+        body: formData,
+    });
+
+    if (!response.ok) return null;
+
+    const data = await response.json();
+    cachedBearerToken = data.access_token; // Cache the token
+    return cachedBearerToken;
 }
